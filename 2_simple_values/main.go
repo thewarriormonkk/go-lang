2f64558c2cb2ab627a7310a2ba4a98100c35754b
Satendra Mishra <msatendra424@gmail.com>
2024-10-02 20:05:14 +0530

    learning go lang

---
+++ 2_simple_values/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	// simple values
+	// int
+	fmt.Println(4)
+	fmt.Println(4 + 9)
+
+	// floats
+	fmt.Println(5.6)
+	fmt.Println(5.0 / 2.5)
+	fmt.Println(5.6 / 2)
+
+	// string
+	fmt.Println("string literal")
+
+	// bool
+	fmt.Println(true)
+	fmt.Println(false)
+
+}
